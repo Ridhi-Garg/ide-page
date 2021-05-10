1c406768c54692147b202db0412dcf66c4a4f494
Ridhi-Garg <82520611+Ridhi-Garg@users.noreply.github.com>
2021-05-10 23:47:52 +0530

    Add files via upload

---
+++ src/components/Editor.js
@@ -0,0 +1,14 @@
+import Form from './Form'
+const Editor = () => {
+    return (
+        <div className="editor" >
+
+                <Form/> 
+                {/* <Textarea readOnly={false} placeholder='Write a code..' /> */}
+            
+
+        </div>
+    )
+}
+
+export default Editor
---
+++ src/components/Sidebar.js
@@ -0,0 +1,20 @@
+import Button from './Button'
+const Sidebar = () => {
+    
+    const onClick =()=>{
+    console.log('click')
+}
+
+    return (
+        <div className='sidebar'>
+
+            <Button color='' text='View' onClick={onClick}/>
+            <Button color='' text='Save' onClick={onClick}/>
+            <Button color='' text='Collaborate' onClick={onClick}/>
+            <Button color='' text='Share' onClick={onClick}/>
+            <Button color='' text='Logout' onClick={onClick}/>
+        </div>
+    )
+}
+
+export default Sidebar
